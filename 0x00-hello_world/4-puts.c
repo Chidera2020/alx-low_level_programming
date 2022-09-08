e6228826281e64e3fd5dfc4b21eeda7bd682b304
Chidera2020 <c.chideranaomi@gmail.com>
2022-09-08 16:13:10 -0700

    now

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ *main-printing
+ *
+ * return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return (0)
+}

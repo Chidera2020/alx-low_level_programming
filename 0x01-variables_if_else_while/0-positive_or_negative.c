5f7671e246dcc48d2517660956142f1d9f62c03b
Chidera2020 <c.chideranaomi@gmail.com>
2022-09-12 15:32:35 -0700

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  * Description: program will assign a random number to the variable n each time
  * it is executed.
  *
- * Return: Always 0 (succcess)
+ * Return: Always 0 (success)
  */
 
 int main(void)

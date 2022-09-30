84a2667e34550531567c20e5bd85911cc05db7c1
Chidera2020 <c.chideranaomi@gmail.com>
2022-09-30 14:44:27 -0700

    tired

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,14 +26,14 @@ int main(int argc, char **argv)
 	for (i = 1; argv[i]; i++)
 	{
 		n = strtol(argv[i], &flag, 10);
-				if (*flag)
-							{
-											printf("Error\n");
-														return (1);
-																}
-						else
-										sum += n;
-							}
-		printf("%d\n", sum);
-			return (0);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
